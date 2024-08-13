fa0a60623950ac930e4acf5f9c92f62b49815ac4
tejesh reddy <tejeshvit007@gmail.com>
2024-08-13 17:12:39 +0530

    integrated /onebox/list api and display list of emails and also added search functionality when typed name or email it display emails related to those name and email and also added refresh functionality when click on it it refresh the emails list and also display real-time new replies

--- src/app/inbox/page.js
+++ src/app/inbox/page.js
@@ -5,14 +5,17 @@ import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
+import Listofemail from "../components/Listofemail";
+import Messages from "../components/Messages";
 
 function Inboxpage() {
   const params = useSearchParams();
   const [isDark, setisDark] = useState(true);
-
-  const [token, setToken] = useState(null); // Start with null to indicate loading
+  const [Emaillist, setEmaillist] = useState(null);
+  // const [token, setToken] = useState(null); // Start with null to indicate loading
   const [isAuthenticatestatus, setisAuthenticatestatus] = useState(null); // Start with null
   const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     //const authToken = Cookies.get("authToken");
@@ -20,8 +23,9 @@ function Inboxpage() {
 
     const tokenFromQuery = params.get("token");
     if (tokenFromQuery) {
-      setToken(tokenFromQuery);
-      Cookies.set("authToken", tokenFromQuery, { expires: 30 });
+      //setToken(tokenFromQuery);
+      Cookies.set("authToken", tokenFromQuery);
+
       setisAuthenticatestatus(true);
     }
   }, [params]);
@@ -35,6 +39,59 @@ function Inboxpage() {
       router.push("/login");
     }
   }, [isAuthenticatestatus, router]);
+  const fetchEmails = async () => {
+    try {
+      const authToken = Cookies.get("authToken");
+      setIsLoading(true);
+
+      const requestOptions = {
+        method: "GET",
+        redirect: "follow",
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      };
+
+      const response = await fetch(
+        "https://hiring.reachinbox.xyz/api/v1/onebox/list",
+        requestOptions
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const res = await response.json();
+
+      // Group emails by fromEmail and keep the latest one based on sentAt
+      const latestEmailsMap = res.data.reduce((acc, email) => {
+        const existingEmail = acc[email.fromEmail];
+
+        if (
+          !existingEmail ||
+          new Date(email.sentAt) > new Date(existingEmail.sentAt)
+        ) {
+          acc[email.fromEmail] = email;
+        }
+
+        return acc;
+      }, {});
+
+      // Convert the map back to an array
+      const latestEmails = Object.values(latestEmailsMap);
+      latestEmails.sort((a, b) => new Date(b.sentAt) - new Date(a.sentAt));
+      setEmaillist({ ...res, data: latestEmails });
+      setIsLoading(false);
+    } catch (error) {
+      console.error("Error fetching emails:", error);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchEmails();
+  }, []);
+
   /*useEffect(() => {
     if (isAuthenticatestatus === false) {
       router.push("/login");
@@ -49,7 +106,21 @@ function Inboxpage() {
     return (
       <div className="flex h-full">
         <Sidebar isDark={isDark} />
-        <Navbar isDark={isDark} setisDark={setisDark} />
+
+        <div className="flex flex-col w-full">
+          <Navbar isDark={isDark} setisDark={setisDark} />
+          <div className="flex h-full w-full">
+            <Listofemail
+              isDark={isDark}
+              Emaillist={Emaillist}
+              fetchEmails={fetchEmails}
+              setEmaillist={setEmaillist}
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
+            />
+            <Messages />
+          </div>
+        </div>
       </div>
     );
   } else {
