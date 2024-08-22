a1c69a985684940a2b356199acc658ebe04d9ae8
tejesh reddy <tejeshvit007@gmail.com>
2024-08-22 15:49:43 +0530

    adding a loader in root directory of src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,13 +2,21 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import { ClipLoader } from "react-spinners";
 
 export default function Home() {
   const router = useRouter();
+  const spinnerStyle = {
+    borderWidth: "5px",
+  };
 
   useEffect(() => {
     router.push("/login");
   }, [router]);
 
-  return null;
+  return (
+    <div className="flex justify-center items-center h-full w-full bg-[#1F1F1F]">
+      <ClipLoader size={35} color="#4285F4" cssOverride={spinnerStyle} />
+    </div>
+  );
 }
